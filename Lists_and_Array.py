6b65b1c7b6a95dbc56fea835fa076d87ce54ca94
SM112223 <sohammaid23@gmail.com>
2025-09-02 19:50:28 +0530

    Starting with python DSA

---
+++ Lists_and_Array.py
@@ -0,0 +1,12 @@
+z = [1, "hello", 3.14, True]
+# lists can contain mixed data types
+
+x = [9, 12, 7, 4, 11]
+
+# Add element to a list
+x.append(8)
+
+# Sort list ascending , x.sort() defaults in ascending order
+x.sort(reverse=True) # used reverse=True to print in descending order
+
+print(x)  

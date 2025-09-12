7f9d9c525025b90d9ecc0746020a04667dcc582d
SM112223 <sohammaid23@gmail.com>
2025-09-12 11:12:04 +0530

    Sorting - bubble sort added

---
+++ Sorting/Bubble_Sort.py
@@ -0,0 +1,22 @@
+# What is sorting ?
+# Sorting is the process of arranging data in a particular format or order.
+# It can be in ascending or descending order.
+
+# Initial unsorted list
+mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+
+# Length of the list
+n = len(mylist)
+
+# Outer loop: controls the number of passes (n-1 passes needed)
+for i in range(n-1):
+
+    # Inner loop: compares elements up to the unsorted portion
+    for j in range(n-i-1):
+
+        # If the current element is greater than the next one, swap them
+        if mylist[j] > mylist[j+1]:
+            mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+
+# Final sorted list
+print(mylist)
